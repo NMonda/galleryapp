8429d37a4a385037dc9d7c3864461027354b910e
NMONDA <ndegemonda@gmail.com>
2023-01-21 10:38:02 +0300

    middleware added

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const express = require('express');
 
 let indexRouter = require('./routes/index');
 let search_router = require('./routes/search');
+
+// Initialize express
+const app = express();
+
+// Set up a view engine
+app.set('view engine', 'ejs');
+
+// Set a static folder
+app.use(express.static('public'));
+
+// formData => req.body
+app.use(express.json()); // application/json
+app.use(express.urlencoded({extended: true}));
+
